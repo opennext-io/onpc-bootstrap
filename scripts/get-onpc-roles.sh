fa09fe00e38759423e9afee697982562bcadfc0b
Patrick Personal <patrick.michel.petit@gmail.com>
2019-06-10 11:25:50 +0200

    Introduction of onpc_proxy_mode logic

--- scripts/get-onpc-roles.sh
+++ scripts/get-onpc-roles.sh
@@ -5,13 +5,11 @@ set -ex
 # Trace everything into specific log file
 exec > >(tee -i /var/log/"$(basename "$0" .sh)"_"$(date '+%Y-%m-%d_%H-%M-%S')".log) 2>&1
 
-if [ ! -r /opt/onpc-bootstrap/ansible-role-requirements.yml ]; then
-	echo "Missing or unreadable file -e role_file=/opt/onpc-bootstrap/ansible-role-requirements.yml"
+if [ ! -r ../ansible-role-requirements.yml ]; then
+	echo "Missing or unreadable file -e role_file=../ansible-role-requirements.yml"
 	exit 1
 fi
 # Import the ansible role dependencies
-pushd /opt/openstack-ansible
-/usr/bin/ansible-playbook ./scripts/get-ansible-role-requirements.yml -e role_file=/opt/onpc-bootstrap/ansible-role-requirements.yml
-popd
+/usr/bin/ansible-playbook /opt/openstack-ansible/scripts/get-ansible-role-requirements.yml -e role_file=../ansible-role-requirements.yml
 # All done
 touch /opt/.onpc-get-roles-done
